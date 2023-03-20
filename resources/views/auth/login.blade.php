992349b65476e072e3aaf220b943ba01259bc9a1
Karma Sherpa <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>
2023-03-20 21:07:42 +0545

    new comment

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,7 +4,7 @@
 
     <form method="POST" action="{{ route('login') }}">
         @csrf
-
+ <!-- Email Address -->
         <!-- Email Address -->
         <div>
             <x-input-label for="email" :value="__('Email')" />
