7fb2a2f2051c49c6c8cedbc50cd22a876daecf54
Karma Sherpa <126855790+KarmaSherpaa@users.noreply.github.com>
2023-03-30 15:19:55 +0545

    dashboard updated

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,40 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
-                </div>
-            </div>
+<link rel="stylesheet" href="{{asset('cssFile/styles.min.css')}}">
+<link rel="shortcut icon" type="image/png" href="img/favicon.ico"/>
+<link rel="icon" type="image/x-icon" href="img/favicon.ico"/>
+
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Fira+Code:wght@500;700&family=Inter:wght@400;700&family=Roboto:wght@300;400;500&display=swap" rel="stylesheet"> 
+
+<title>TMS</title>
+<meta name="title" content="TMS">
+
+</head>
+
+<body class=" dashboard ">
+    <x-sidebar></x-sidebar>
+        <div class="inner">
+            
+<div class="text-block">
+    <h1>Dashboard</h1>
+    <p>Welcome to the Tax Management System.</p>
+
+    
+</div>
+
         </div>
     </div>
-</x-app-layout>
+
+</body>
+</html>
+
+
+<script type="text/javascript" src="{{asset('js/bootstrap.bundle.min.js')}}"></script>
+<script type="text/javascript" src="{{asset('js/jquery-3.6.1.min.js')}}"></script>
+<script type="text/javascript" src="{{asset('js/app.min.js')}}"></script>
