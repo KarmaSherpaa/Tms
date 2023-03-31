e459305de209a9d0fa971ad0a45d3eefe7de821a
Karma Sherpa <126855790+KarmaSherpaa@users.noreply.github.com>
2023-03-31 22:14:10 +0545

    Backend project

---
+++ resources/views/project.blade.php
@@ -0,0 +1,148 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<link rel="stylesheet" href="{{asset('cssFile/styles.min.css')}}">
+<link rel="shortcut icon" type="image/png" href="img/favicon.ico"/>
+<link rel="icon" type="image/x-icon" href="img/favicon.ico"/>
+
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Fira+Code:wght@500;700&family=Inter:wght@400;700&family=Roboto:wght@300;400;500&display=swap" rel="stylesheet"> 
+
+<title>TMS</title>
+
+<meta name="title" content="TMS">
+
+</head>
+
+<body class=" dashboard ">
+    <x-sidebar></x-sidebar>
+        <div class="inner">
+            
+<div class="project-block">
+    <div class="project-result">
+        <p>33,849 total results, <a href="#">Export project list</a></p>
+    </div>
+    <div class="project-item">
+        <div class="row justify-content-between">
+            <div class="col-xl-6">
+                <p class="lastedit">Last modified 23m ago</p>
+                <h2 class="project-title"><a href="">Waterside Hotel</a></h2>
+                <p>Project #DCA9CE8F, info@efficientenergychoices.com.au</p>
+                <p>50 Bruce St, Toorak 3142</p>
+                <p>BESS-6 - Single dwelling, Score 26%</p>
+            </div>
+            <div class="col-auto mt-1 mt-lg-0">
+                <a href="#" class="btn btn-site">Copy</a>
+                <a href="#" class="btn btn-site">Calc Reports</a>
+                <a href="#" class="btn btn-site">Recalculate</a>
+                <a href="#" class="btn btn-site btn-delete">Delete</a>
+            </div>
+        </div>
+    </div>
+    <div class="project-item">
+        <div class="row justify-content-between">
+            <div class="col-xl-6">
+                <p class="lastedit">Last modified 23m ago</p>
+                <h2 class="project-title"><a href="">Waterside Hotel</a></h2>
+                <p>Project #DCA9CE8F, info@efficientenergychoices.com.au</p>
+                <p>50 Bruce St, Toorak 3142</p>
+                <p>BESS-6 - Single dwelling, Score 26%</p>
+                <div class="revision-wrap">
+                    <p class="submitted revision-toggle">
+                        <svg class="icon-check" width="12" height="11">
+                            <use href="#icon-check"></use>
+                        </svg>
+                        <a href="#"> Submitted, Show 2 revisions</a>
+                    </p>
+                    <ul class="revision-body">
+                        <li><a href="#">Open Revision 2, 7 Jul 2022,Published, Score 55%</a></li>
+                        <li><a href="#">Open Revision 1, 30 Jun 2022, Published, Score 53%</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-auto  mt-1 mt-lg-0">
+                <a href="#" class="btn btn-site">Copy</a>
+                <a href="#" class="btn btn-site">Calc Reports</a>
+                <a href="#" class="btn btn-site">Recalculate</a>
+                <a href="#" class="btn btn-site btn-delete">Delete</a>
+            </div>
+        </div>
+    </div>
+
+    <div class="project-item">
+        <div class="row justify-content-between">
+            <div class="col-xl-6">
+                <p class="lastedit">Last modified 23m ago</p>
+                <h2 class="project-title"><a href="">Waterside Hotel</a></h2>
+                <p>Project #DCA9CE8F, info@efficientenergychoices.com.au</p>
+                <p>50 Bruce St, Toorak 3142</p>
+                <p>BESS-6 - Single dwelling, Score 26%</p>
+                <div class="revision-wrap">
+                    <p class="submitted revision-toggle">
+                        <svg class="icon-check" width="12" height="11">
+                            <use href="#icon-check"></use>
+                        </svg>
+                        <a href="#"> Submitted, Show 2 revisions</a>
+                    </p>
+                    <ul class="revision-body">
+                        <li><a href="#">Open Revision 2, 7 Jul 2022,Published, Score 55%</a></li>
+                        <li><a href="#">Open Revision 1, 30 Jun 2022, Published, Score 53%</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-auto  mt-1 mt-lg-0">
+                <a href="#" class="btn btn-site">Copy</a>
+                <a href="#" class="btn btn-site">Calc Reports</a>
+                <a href="#" class="btn btn-site">Recalculate</a>
+                <a href="#" class="btn btn-site btn-delete">Delete</a>
+            </div>
+        </div>
+    </div>
+    <div class="project-item">
+        <div class="row justify-content-between">
+            <div class="col-xl-6">
+                <p class="lastedit">Last modified 23m ago</p>
+                <h2 class="project-title"><a href="">Waterside Hotel</a></h2>
+                <p>Project #DCA9CE8F, info@efficientenergychoices.com.au</p>
+                <p>50 Bruce St, Toorak 3142</p>
+                <p>BESS-6 - Single dwelling, Score 26%</p>
+                <div class="revision-wrap">
+                    <p class="submitted revision-toggle">
+                        <svg class="icon-check" width="12" height="11">
+                            <use href="#icon-check"></use>
+                        </svg>
+                        <a href="#"> Submitted, Show 2 revisions</a>
+                    </p>
+                    <ul class="revision-body">
+                        <li><a href="#">Open Revision 2, 7 Jul 2022,Published, Score 55%</a></li>
+                        <li><a href="#">Open Revision 1, 30 Jun 2022, Published, Score 53%</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-auto  mt-1 mt-lg-0">
+                <a href="#" class="btn btn-site">Copy</a>
+                <a href="#" class="btn btn-site">Calc Reports</a>
+                <a href="#" class="btn btn-site">Recalculate</a>
+                <a href="#" class="btn btn-site btn-delete">Delete</a>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+        </div>
+    </div>
+
+</body>
+
+
+</html>
+
+
+<script type="text/javascript" src="{{asset('js/bootstrap.bundle.min.js')}}"></script>
+<script type="text/javascript" src="{{asset('js/jquery-3.6.1.min.js')}}"></script>
+<script type="text/javascript" src="{{asset('js/app.min.js')}}"></script>
