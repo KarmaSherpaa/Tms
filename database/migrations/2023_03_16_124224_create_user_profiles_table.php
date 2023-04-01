3a45999d8764afe9dc54915ba0a56afefbc110b2
Karma Sherpa <126855790+KarmaSherpaa@users.noreply.github.com>
2023-04-01 23:49:25 +0545

    database updated

--- database/migrations/2023_03_16_124224_create_user_profiles_table.php
+++ database/migrations/2023_03_16_124224_create_user_profiles_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('user_profiles', function (Blueprint $table) {
             $table->id();
             $table->foreignId('province_id')->constrained('province_infos');
+            $table->foreignId('user_id')->constrained('users');
             $table->timestamps();
         });
     }
