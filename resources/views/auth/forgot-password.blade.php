4e0bfe0644e43ace7e4af0637b72cca2d1f48a6a
Karma Sherpa <126855790+KarmaSherpaa@users.noreply.github.com>
2023-04-01 23:53:29 +0545

    forgot password modified

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,25 +1,45 @@
-<x-guest-layout>
-    <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
-        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+<header>
+        
+    <div class="logo">
+      <a href="#"><img src="{{asset('Images/logo.svg')}}" alt="logo"></a>
+      
     </div>
 
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
+    <div class="button">
+      <a href="{{ route('register') }}" class="btn btn-1">Sign up</a>
+      <a href="{{ route('login') }}" class="btn btn-2">Sign in</a>
 
-    <form method="POST" action="{{ route('password.email') }}">
-        @csrf
+    </div> 
+  </header>
+  <x-guest-layout>
 
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
+ 
+<link rel="stylesheet" href="{{asset('cssFile/ForgotPassword.css')}}">
 
-        <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
-                {{ __('Email Password Reset Link') }}
-            </x-primary-button>
-        </div>
-    </form>
+<!-- Session Status -->
+<x-auth-session-status class="mb-4" :status="session('status')" />
+
+<form method="POST" action="{{ route('password.email') }}">
+    @csrf
+    <div class="forgot-box">
+    <h2 style="font-size: larger" >Forgot Password</h2>
+    <hr  class="h">
+    <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
+      {{ __('let us know your email address and we will email you a password reset link.') }}
+    </div>
+    
+    <!-- Email Address -->
+    <div style="margin-right:35%">
+        <x-input-label for="email" :value="__('Email')" />
+        <x-text-input style="width:150%"  id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+        <x-input-error :messages="$errors->get('email')" class="mt-2" />
+    </div>
+
+    <div class="forgotbutton">
+        <x-primary-button>
+            {{ __('Forgot Password') }}
+        </x-primary-button>
+    </div>
+</form>
+</div>
 </x-guest-layout>
