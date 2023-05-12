100da07faab691ec269059866024b357a9604c15
Karma Sherpa <126855790+KarmaSherpaa@users.noreply.github.com>
2023-05-13 03:45:50 +0545

    migration

---
+++ database/migrations/2023_05_04_142748_create_bands_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('bands', function (Blueprint $table) {
+            $table->id();
+            $table->string('m_status',50);
+            $table->string('band_type',50);
+            $table->string('vlaue',50);
+            $table->integer('rate');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('bands');
+    }
+};
