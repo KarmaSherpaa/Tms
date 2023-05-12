63c3069298c4951b4dab3b6f73f2fdb569b7a07f
Karma Sherpa <126855790+KarmaSherpaa@users.noreply.github.com>
2023-05-13 00:03:45 +0545

    Band Model

---
+++ app/Models/Band.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Band extends Model
+{
+    use HasFactory;
+    protected $fillable =['m_status','band_type','value','rate'];
+}
