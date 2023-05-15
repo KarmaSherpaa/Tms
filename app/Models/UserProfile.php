bde0781dbed71fb4a741e5b702946066b579137b
Sagar Bhandari <127657613+sagarbh12@users.noreply.github.com>
2023-05-15 12:09:17 +0545

    UserProfile

--- app/Models/UserProfile.php
+++ app/Models/UserProfile.php
@@ -5,11 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+
 class UserProfile extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['province_id','user_id','phone'];
+    protected $fillable = ['province_id','user_id','phone','first_name','last_name','dob','gender','nationality','status','grandfather_name','father_name','mother_name','spouse_name','citizenship_numer','citizenship_issue_date','citizenship_documents','citizenship_documents_back','country','user_province','district','municipality','ward','tole','profile_picture','pan_number'];
 
     public function user(){
         return $this->belongsTo(User::class);
@@ -19,3 +20,13 @@ class UserProfile extends Model
     }
 
 }
+
+
+ 
+
+
+
+
+
+
+
