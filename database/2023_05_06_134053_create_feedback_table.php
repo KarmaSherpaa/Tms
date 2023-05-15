12c4e69c3e65291f0d3f0338e0e6af0575046b59
Sagar Bhandari <127657613+sagarbh12@users.noreply.github.com>
2023-05-15 12:14:13 +0545

    Feedback table

---
+++ database/2023_05_06_134053_create_feedback_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('feedback', function (Blueprint $table) {
+            $table->id();
+            $table->string('name',50);
+            $table->string('email',50);
+            $table->string('subject',50);
+            $table->string('message',50);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('feedback');
+    }
+};
