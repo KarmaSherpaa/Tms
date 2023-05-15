405fd46cb5fb5474f791eb320676da223f6d0a24
Sagar Bhandari <127657613+sagarbh12@users.noreply.github.com>
2023-05-15 12:15:09 +0545

    add citizenship documents

---
+++ database/2023_05_08_111955_add_citizenship_documents_back_and_profile_picture_columns_to_user_profiles_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('user_profiles', function (Blueprint $table) {
+            $table->string('citizenship_documents_back',191)->nullable();
+            $table->string('profile_picture',191)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('user_profiles', function (Blueprint $table) {
+           //
+        });
+    }
+};
